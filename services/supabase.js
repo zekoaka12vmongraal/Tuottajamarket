acbbf491f9812600e8fdc12a47df5eaba15c2d7b
Alluu17 <150897351+zekoaka12vmongraal@users.noreply.github.com>
2025-12-09 22:28:41 +0200

    Add Supabase configuration file

--- services/supabase.js
+++ services/supabase.js
@@ -4,6 +4,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_KEY) {
+  console.error("Missing Supabase environment variables!");
+}
+
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
