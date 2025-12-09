22d54e357f6d33a32f66776e748e54c1a570c23d
Alluu17 <150897351+zekoaka12vmongraal@users.noreply.github.com>
2025-12-10 00:37:20 +0200

    fix: korjaa appr.jsx ja muut bugit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,48 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./components/CartContext";
 import { LanguageProvider } from "./components/LanguageContext";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CartSidebar from "./components/CartSidebar";
+
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import Checkout from "./pages/Checkout";
 
+function AppContent() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <Header />
+      <CartSidebar />
+
+      <main style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <LanguageProvider>
-      <CartProvider>
+    <CartProvider>
+      <LanguageProvider>
         <BrowserRouter>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              minHeight: "100vh",
-            }}
-          >
-            <Header />
-            <CartSidebar />
-            <main style={{ flex: 1 }}>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/store" element={<Store />} />
-                <Route path="/checkout" element={<Checkout />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
+          <AppContent />
         </BrowserRouter>
-      </CartProvider>
-    </LanguageProvider>
+      </LanguageProvider>
+    </CartProvider>
   );
 }
